bfa15ba0196044509a26260c58f4e92193af7066
koehrlenguillaume <koehrlen.guillaume@gmail.com>
2014-10-02 11:11:14 +0200

    Gestion slider

---
+++ js/inscription.js
@@ -0,0 +1,24 @@
+
+$("#formInscription input").focusout(function(e){
+
+    var element = $("#"+$(e.target).attr("id"));
+
+    switch (element.attr("id"))
+    {
+        case "email2":
+            if($("#email").val().trim() != $("#email2").val().trim())
+            	$("#email2").popover('show');
+			else
+				$("#email2").popover('destroy');
+            break;
+        case "pwd2":
+            if($("#pwd").val().trim() != $("#pwd2").val().trim())
+            	$("#pwd2").popover('show');
+			else
+				$("#pwd2").popover('destroy');
+            break;
+        default :
+            break;
+    }
+});
+
