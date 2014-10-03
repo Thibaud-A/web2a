4af77e63fb76576f35db47d7dac35334b1ebb268
koehrlenguillaume <koehrlen.guillaume@gmail.com>
2014-10-03 16:34:27 +0200

    ajout panier

--- js/inscription.js
+++ js/inscription.js
@@ -7,13 +7,13 @@ $("#formInscription input").focusout(function(e){
     {
         case "email2":
             if($("#email").val().trim() != $("#email2").val().trim())
-            	$("#email2").popover('show');
+            	$("#email2").popover('show',"bottom");
 			else
 				$("#email2").popover('destroy');
             break;
         case "pwd2":
             if($("#pwd").val().trim() != $("#pwd2").val().trim())
-            	$("#pwd2").popover('show');
+            	$("#pwd2").popover('show',"bottom");
 			else
 				$("#pwd2").popover('destroy');
             break;
